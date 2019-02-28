1aaed8690a399a202e2bf1eec7a70df776631563
tanyamn <tmn051@ucsd.edu>
2019-02-28 10:39:11 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = "a8X99FYHNeGUu4P7Pb7PPYWL7E3TG0Vc";
+
+	//map refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.878973, -117.235925],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	//add a marker to the map
+	L.marker([32.878973, -117.235925]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,13 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response) {
+	var rsvpEmail = request.body.rsvpEmail;
+	//This will print to your terminal whenever the POST request is made
+	console.log(rsvpEmail);
+
+	data.rsvp.push(rsvpEmail);
+
+	response.send(rsvpEmail);
+};
